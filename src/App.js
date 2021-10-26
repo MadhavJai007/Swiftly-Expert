b6f14ed5af2975ca09b898f15242647521ffb7e0
Madhav Jaisankar <madhav.jaisankar@gmail.com>
2021-10-26 12:14:22 -0400

    added and configured styling framework and tools

--- src/App.js
+++ src/App.js
@@ -4,24 +4,45 @@ import React, {Component, useState, useEffect, useRef} from 'react';
 
 
 const App = () => {
+
+  // mimics component.onMount
+  useEffect(() => {
+    document.title = "Swiftly";
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Make <code>src/App.js</code> the splash screen (shows Swiftly logo and stuff).
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    </>
+    // <div className="App" > 
+    //   <div className="bg-purple-600 bg-opacity-100">
+
+    //   </div>
+    //   {/* <h1 className="text-red-500">Heyyyyy</h1> */}
+    //   {/* <header className="App-header"> */}
+    //   {/* <header style={backgroundStyle}>
+        
+    //     <p className="text-red-500">
+    //       Make <code>src/App.js</code> the splash screen (shows Swiftly logo and stuff).
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    // </div>
   );
+}
+
+const backgroundStyle = {
+  backgroundColor: '#282828',
+  minHeight: "100vh",
+  display: "flex"
 }
 
 export default App;
