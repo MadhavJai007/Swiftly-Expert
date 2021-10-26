fe6ce37e8ab40578e5282404b6de7b654374803e
Madhav Jaisankar <madhav.jaisankar@gmail.com>
2021-10-26 05:09:42 -0400

    Initialized swiftly expert

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component, useState, useEffect, useRef} from 'react';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Make <code>src/App.js</code> the splash screen (shows Swiftly logo and stuff).
         </p>
         <a
           className="App-link"
