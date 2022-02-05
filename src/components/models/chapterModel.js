1d0ac359eba9a92f5833dd8c2734b7a8f9f72fc6
Madhav Jaisankar <madhav.jaisankar@gmail.com>
2022-02-05 07:10:23 -0500

    added some comments

--- src/components/models/chapterModel.js
+++ src/components/models/chapterModel.js
@@ -1,6 +1,7 @@
 // model of the chapter
 // and other miscellaneous objects needed for the dashboard
 
+// template object for a chapter entity. Used for making instances of chapter objects and storing new and modified instances in firebase
 export const chapterObj = {
     chapter_id: "",
     chapter_title: "",
@@ -15,6 +16,8 @@ export const chapterObj = {
     playground: [] // playgrounds subcollection
 }
 
+// similarly to the chapter object, this is a lesson model that can be used to create a lesson entity. The id, title and content are the properties of this model.
+// lesson content is an array that will the a list of strings. Each string represents a text block that can be a paragraph of refgular text or a code snippet
 export const templateLesson = {
     lesson_id: '',
     lesson_title: '',
